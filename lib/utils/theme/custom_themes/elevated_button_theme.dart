6f38b611de3e48f62d6c5274a730ab4972421c64
Adam Masha <adamngowa3@gmail.com>
2026-02-04 13:50:06 +0300

    theme setup

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+///Light and Dark Elevated Button Themes
+class TElevatedButtonTheme {
+  TElevatedButtonTheme._(); //To avoid creating instances
+  ///Light Elevated Button Theme
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: BorderSide(color: Colors.blue),
+      padding: EdgeInsets.symmetric(vertical: 18),
+      textStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+
+  ///Dark Elevated Button Theme
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: BorderSide(color: Colors.blue),
+      padding: EdgeInsets.symmetric(vertical: 18),
+      textStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.black,
+        fontWeight: FontWeight.w600,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+}
