3646f21dd614a412bcfa3b3994d5c61e49affe62
Adam Masha <adamngowa3@gmail.com>
2026-02-11 22:55:23 +0300

    navigation bar 2.0

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,16 +7,39 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //creating an instance of navigation controller
+    //now we can use whatever is inside navigation controller using "controller" variable
+    final controller = Get.put(NavigationController());
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
-          NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
-          NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
-          NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
-        ],
+      //Obx widget allows flutter to redraw the screen when changes are detected
+      //Obx is observer observing e.g Rx<int> a = 0.obs(the observerd)
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+
+          destinations: [
+            NavigationDestination(icon: Icon(Iconsax.home), label: "Home"),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: "Store"),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: "Wishlist"),
+            NavigationDestination(icon: Icon(Iconsax.user), label: "Profile"),
+          ],
+        ),
       ),
-      body: Container(),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+  final screens = [
+    Container(color: Colors.amber),
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+    Container(color: Colors.purple),
+  ];
 }
