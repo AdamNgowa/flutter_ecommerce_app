c3842a5271d234e155675d88d035a70e2d1e0c64
Adam Masha <adamngowa3@gmail.com>
2026-02-13 21:13:59 +0300

    removed unused imports

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecomm_app/common/widgets/appbar/appbar.dart';
 import 'package:flutter_ecomm_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
-import 'package:flutter_ecomm_app/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:flutter_ecomm_app/features/shop/screens/home/widgets/home_appbar.dart';
-import 'package:flutter_ecomm_app/utils/constants/colors.dart';
-import 'package:flutter_ecomm_app/utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
